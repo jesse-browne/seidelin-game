d740d16f1b10a78538381297bf261e920f91b826
Jesse Browne <jb@greenpag.es>
2013-07-29 12:37:07 +1000

    Added selectJewel() to screen.game.js

--- scripts/screen.game.js
+++ scripts/screen.game.js
@@ -4,17 +4,53 @@
 
 jewel.screens['game-screen']= (function() {
 	var board = jewel.board,
-	    display = jewel.display;
+	    display = jewel.display,
+	    cursor;
 	
 	function run() {
 		board.initialize(function() {
 			display.initialize(function() {
-				display.redraw(board.getBoard(), function() {
-					console.log('Do nothing for now');
-					// do nothing for the moment
-				});
+				cursor = {
+				    x : 0,
+				    y : 0,
+				    selected : false
+				};
+				display.redraw(board.getBoard(), function() {});
 			});
 		});
+	}
+	
+	function setCursor(x, y, select) {
+		cursor.x = x;
+		cursor.y = y;
+		cursor.selected = select;
+	}
+	
+	function selectJewel(x, y) {
+		if (arguments.length == 0) {
+			selectJewel(cursor.x, cursor.y);
+			return;
+		}
+		
+		if (cursor.selected) {
+			var dx = Math.abs(x - cursor.x),
+			    dy = Math.abs(y - cursor.y),
+			    dlist = dx +dy;
+			
+			if (dist == 0) {
+				// deselected the selected jewel
+				setCursor(x, y, false);
+			} else if (dist == 1) {
+				// selected an adjacent jewel
+				board.swap(cursor.x, cursor.y, x, y, playBoardEvents);
+				setCursor(x, y, false);
+			} else {
+				// selected a different jewel
+				setCursor(x, y, true);
+			}
+		} else {
+			setCursor(x, y, true);
+		}
 	}
 	
 	return {
