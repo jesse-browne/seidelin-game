ed65fcd2edae15234dad4c7e3afe986d44caac64
Jesse Browne <jb@greenpag.es>
2013-08-02 13:36:35 +1000

    High Score screen works now!

--- scripts/screen.hiscore.js
+++ scripts/screen.hiscore.js
@@ -29,10 +29,60 @@ jewel.screens['hiscore'] = (function() {
 	}
 	
 	function populateList() {
+		var scores = getScores(),
+		    list = $('#hiscore ol.score-list')[0],
+		    item,
+		    nameEl,
+		    scoreEl,
+		    i;
 		
+		for (var i = scores.length; i < numScores; i++) {
+			scores.push({
+				name : '---',
+			    score : 0
+			});
+		}
+		
+		list.innerHTML = '';
+		
+		for (i = 0; i < scores.length; i++) {
+			item = document.createElement('li');
+			
+			nameEl = document.createElement('span');
+			nameEl.innerHTML = scores[i].name;
+			
+			scoreEl = document.createElement('span');
+			scoreEl.innerHTML = scores[i].score;
+			
+			item.appendChild(nameEl);
+			item.appendChild(scoreEl);
+			list.appendChild(item);
+		}
+	}
+	
+	function getScores() {
+	    return storage.get('hiscore') || [];
 	}
 	
 	function enterScore(score) {
+		var scores = getScores(),
+		    name,
+		    i,
+		    entry;
+		
+		for (i = 0; i <= scores.length; i++) {
+			if (i == scores.length || score > scores[i].score) {
+				name = prompt('Please enter your name:');
+				entry = {
+					name : name,
+					score : score
+				};
+				scores.splice(i , 0, entry);
+				storage.set('hiscore', scores.slice(0, numScores));
+				populateList();
+				return;
+			}
+		}
 		
 	}
 	
