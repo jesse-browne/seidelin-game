10ff634e347dfc92a9b0c4988769d53fc800a0ba
Jesse Browne <jb@greenpag.es>
2013-11-08 13:56:39 -0800

    Added another test to Screen.Game.Spec.js

--- tests/jasmine-standalone-1.3.0/spec/Screen.Game.Spec.js
+++ tests/jasmine-standalone-1.3.0/spec/Screen.Game.Spec.js
@@ -1,9 +1,15 @@
 describe('Screen.Game', function() {
     var firstRun = jewel.screens['game-screen'].firstRun;
-	
+	var paused = jewel.screens['game-screen'].paused;
+    
     it('has a first run boolean which should start as true, helps take care of some setup', function() {
     	expect(firstRun).toBe(true);
-    }); 
+    });
+    
+    it('has a paused boolean which should start as false', function() {
+    	expect(paused).not.toBe(true);
+    });
+    
 });
 
 
